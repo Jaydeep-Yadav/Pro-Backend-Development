0c2ab392c2b322975f4ad1670c98af14182abf13
Jaydeep Yadav <yadavjay374@gmail.com>
2022-02-17 00:16:14 +0530

    insta and linkedin route

--- Social-app/index.js
+++ Social-app/index.js
@@ -8,6 +8,28 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
 
+app.get("/api/v1/insta",(req,res)=>{
+    const instaSocial = {
+        userName : "bad_boy_official2",
+        followers : 942,
+        follows: 684,
+        date: Date.now()
+    };
+
+    res.status(200).json( instaSocial );
+});
+
+app.get("/api/v1/linkedin",(req,res)=>{
+    const instaSocial = {
+        userName : "Jaydeep Yadav",
+        followers : 4324,
+        follows: 100,
+        date: Date.now()
+    };
+
+    res.status(200).json( instaSocial );
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
